102d1913a2e0a59761f3158dc0b9a5ec93d19e64
Samuel-dot-cloud <successam336@gmail.com>
2020-02-14 11:58:04 +0300

    changes to html page

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
 });
-var detail = document.document.getElementById("birthday").value;
+var detail = document.document.getElementById("birthday").innerHTML = d;
 var detail = new Date();
 detail.setDate(1);
 detail.setFullYear(2001);
