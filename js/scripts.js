5a13dec947c25fceca54e68859fc931016a8ae15
Samuel-dot-cloud <successam336@gmail.com>
2020-02-14 11:45:12 +0300

    changes to javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
 });
+var detail = document.document.getElementById("birthday").value;
+var detail = new Date();
+detail.setDate(1);
+detail.setFullYear(2001);
+detail.setMonth(5);
